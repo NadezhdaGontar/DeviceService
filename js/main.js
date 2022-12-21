5f71fd97e7a94562b544b13e5b5fd3c3014bae99
NadezhdaGontar <92990774+NadezhdaGontar@users.noreply.github.com>
2022-12-21 13:30:04 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(function(){
-  
-
-$('.menu__btn').click(function(event){
+  $('.about__slider').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  })
+/*
+  $('.menu__btn').click(function(event){
   $('.menu__btn, .nav').toggleClass('active');
 
 $('body').toggleClass('lock')
@@ -19,4 +24,62 @@ $('body').toggleClass('lock')
 		//анимируем переход на расстояние - top за 1500 мс
 		$('body,html').animate({scrollTop: top}, 4000);
 	});
+
+*/
+
+
+
+
+
+	// 1. Скрыть все content box
+        contentBoxes.forEach(function (item) {
+        item.classList.add('hidden');
+        
+
+      });
+      // 2. Выбрать нужный content box и показать его
+        
+        const contentBox = document.querySelector('#' + this.dataset.tab);
+        contentBox.classList.remove('hidden');
+    });     
+
+
+
+
+const headers = document.querySelectorAll("[data-name='accordeon-title']");
+
+headers.forEach(function (item) {
+    item.addEventListener('click', showContent);
 });
+
+function showContent() {
+    this.nextElementSibling.classList.toggle('hidden');
+}
+/*новый вариант бургер меню*/
+
+
+
+function burgerMenu(selector) {
+  let menu =  $(selector);
+  let button = menu.find('.header__menu-btn');/*передаем кнопку*/
+  let links = menu.find('.header__menu-link');/*передаем класс ссылка*/
+  
+
+  button.on('click', (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  links.on('click', () => toggleMenu());
+ 
+
+  function toggleMenu(){
+    menu.toggleClass('active');/*класс, который добавляется чтобы работало меню*/
+
+    if(menu.hasClass('active')) { /*класс, который добавляется чтобы работало меню*/
+      $('body').css('overflow', 'hidden');
+    }else {
+      $('body').css('overflow', 'visible');
+    }
+  }
+}
+burgerMenu('.header__menu-inner');/*передаем класс обертки, которая содержит кнопку и меню*/
